b3d764a0a397798940eb3c84b6efcdbe854149a0
Magnus Teschner <magnus.2002.mt@gmail.com>
2023-11-21 12:14:14 +0100

    Change grid size with slider

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const container = document.querySelector('.grid-container')
 
-for (let i = 0; i < 256; i++){
+function appendSquare(oneSquareSize){
     const new_div = document.createElement('div');
     new_div.classList.add("in-grid");
+    new_div.style.width = `${oneSquareSize}px`;
+    new_div.style.height = `${oneSquareSize}px`;
     container.appendChild(new_div)
+
+}
+
+
+function clearGrid() {
+    // Remove all child elements (squares) from the container
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function createGrid(size){
+    clearGrid();
+    oneSquareSize = 960 / size;
+    console.log(oneSquareSize);
+    for (let i = 0; i < size**2; i++){
+        appendSquare(oneSquareSize);
+    }
 
 
 }
+
+
+createGrid(45);
+addSquareEventListener();
 
 
 let isDrawing = false;
@@ -14,12 +38,22 @@ let isDrawing = false;
 container.addEventListener('mousedown', () => isDrawing = true);
 container.addEventListener('mouseup', () => isDrawing = false);
 
+const slider = document.querySelector('#myRange');
+const para = document.querySelector('.show-range');
+slider.addEventListener('change', () => {
+    para.textContent = `${slider.value} x ${slider.value}`
+    createGrid(+slider.value);
+    addSquareEventListener();
+
+})
 
 
-allSquares = document.querySelectorAll('.in-grid');
-allSquares.forEach(square => {square.addEventListener('mouseover', ()=> {
-    if (isDrawing){
-        square.style.backgroundColor = "black";
-    } 
-})   
-});
+function addSquareEventListener(){
+    allSquares = document.querySelectorAll('.in-grid');
+    allSquares.forEach(square => {square.addEventListener('mouseover', ()=> {
+        if (isDrawing){
+            square.classList.add("colored-background")
+        } 
+    })   
+    });
+}
